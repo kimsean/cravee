8a34c0605ec11ebb3aefe33ba95d0857e052cec5
Kim Sean <kshonne.developer@gmail.com>
2019-07-25 23:40:54 +0800

    Updates

---
+++ precache-manifest.f2f2f44d08bbff9a1c0dd04c376d1679.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "070ebd8ad8048252e7d30a5b47faf567",
+    "url": "/cravee/index.html"
+  },
+  {
+    "revision": "7d68793ad988238a258c",
+    "url": "/cravee/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b7bec5cd83cd95a1005f",
+    "url": "/cravee/static/css/main.fc42ff91.chunk.css"
+  },
+  {
+    "revision": "7d68793ad988238a258c",
+    "url": "/cravee/static/js/2.44df593b.chunk.js"
+  },
+  {
+    "revision": "b7bec5cd83cd95a1005f",
+    "url": "/cravee/static/js/main.f268b968.chunk.js"
+  },
+  {
+    "revision": "b732c81a088b48fc9405",
+    "url": "/cravee/static/js/runtime~main.2321d4ab.js"
+  },
+  {
+    "revision": "9c84627798e497084cb06c7a7e6256f9",
+    "url": "/cravee/static/media/all.9c846277.svg"
+  },
+  {
+    "revision": "c5013cc22b35c05525740e4c049e2e39",
+    "url": "/cravee/static/media/beef.c5013cc2.svg"
+  },
+  {
+    "revision": "039e63158e9e6edf9d72f384dbcb0a86",
+    "url": "/cravee/static/media/beer.039e6315.svg"
+  },
+  {
+    "revision": "09c8c2f87900abf974b4b3be101118b7",
+    "url": "/cravee/static/media/chicken.09c8c2f8.svg"
+  },
+  {
+    "revision": "0c7d8867b5fbcadcbdc43d7eeeb3d8e6",
+    "url": "/cravee/static/media/coffee.0c7d8867.svg"
+  },
+  {
+    "revision": "4baa5277d5777e49e157add839dbeece",
+    "url": "/cravee/static/media/crab.4baa5277.svg"
+  },
+  {
+    "revision": "a9ef106e0823eb1ec11b03b4cc5d5968",
+    "url": "/cravee/static/media/pizza.a9ef106e.svg"
+  },
+  {
+    "revision": "70ff5827cdf4d43619e5ee7c2ba77d58",
+    "url": "/cravee/static/media/ramen.70ff5827.svg"
+  },
+  {
+    "revision": "663465debce2f8d97fc240a14662a1ef",
+    "url": "/cravee/static/media/shawarma.663465de.svg"
+  },
+  {
+    "revision": "073709082c372a5d89322d5f27e8cb35",
+    "url": "/cravee/static/media/shrimp.07370908.svg"
+  }
+]);
